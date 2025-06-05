b871dbbba02d93819da9d7ecd00611e19444d436
jinjuho2 <jinjuho3@gmail.com>
2025-06-05 14:53:23 +0900

    feat: Base 완성

---
+++ Assets/Scripts/RotateCharacter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateCharacter : MonoBehaviour , IDragHandler , IEndDragHandler
+{
+    public GameObject character;
+    private float rotateSpeed = 5f;
+    private Vector3 rot;
+    [SerializeField]
+    private Vector3 origin;
+
+    void Start()
+    {
+        rot = character.transform.localEulerAngles; // 캐릭터 로컬 각도
+        origin = rot;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rot.y += Input.GetAxis("Mouse X") * rotateSpeed ;
+        character.transform.localEulerAngles = -rot;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        character.transform.localEulerAngles = origin;
+    }
+
+}
